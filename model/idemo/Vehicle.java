a10ae2205357f347617f7b4d5ca341769b2c9510
Arthiupperla <arthiupperla1297@gmail.com>
2021-10-08 15:27:30 +0530

    Initial Commit

---
+++ model/idemo/Vehicle.java
@@ -0,0 +1,37 @@
+package model.idemo;
+
+public abstract class Vehicle {
+     
+    private int x;
+    private int y;
+    private String make;
+    private double price;
+
+    public Vehicle(int x, int y, String make, double price){
+        this.x=x;
+        this.y=y;
+        this.make=make;
+        this.price=price;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    public String getMake() {
+        return make;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "vehicle at(" + x +", " + y +") " +
+            make + " $ " + price;
+    }
+}
---
+++ model/idemo/car.java
@@ -0,0 +1,20 @@
+package model.idemo;
+
+public class car extends Vehicle {
+
+    private String type;
+
+    public car(int x, int y, String make, double price, String type) {
+        super(x, y, make, price);
+        this.type = type;     
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "[car] "+ super.toString() + ", type =" + type;
+    }
+}
